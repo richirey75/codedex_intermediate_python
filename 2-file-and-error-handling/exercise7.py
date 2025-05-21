67ea1dfe57de4b58544c113167bd9a5e7f05430a
Richard <richirey75@gmail.com>
2025-05-21 14:19:17 -0700

    exercise 7

--- 2-file-and-error-handling/exercise7.py
+++ 2-file-and-error-handling/exercise7.py
@@ -4,6 +4,7 @@
 # 'w' - write
 # 'a' - append
 
+'''
 file = open('example.txt', 'r')
 
 content = file.read()
@@ -18,3 +19,30 @@ line2 = file.readline()  # Read the second line
 print(line2, end='')     # Printing the second line
 
 file.close()
+
+'''
+liked_songs = {
+  'Bad Habits': 'Ed Sheeran',
+  'I\'m Just Ken': 'Ryan Gosling',
+  'Mastermind': 'Taylor Swift',
+  'Uptown Funk': 'Mark Ronson ft. Bruno Mars',
+  'Ghost': 'Justin Bieber'
+}
+
+
+def write_liked_songs_to_file(songs, file_name):
+  with open(file_name, 'w') as file:
+    file.write('Liked songs: \n')
+    for song, artist in songs.items(): # songs is  the key and artist is the value, I did not know this, so keep in mind
+      file.write(f'{song} by {artist}\n')
+  print(f"Successfully added Liked songs to {file_name} ❤️")
+
+
+write_liked_songs_to_file(liked_songs, 'liked_songs.txt')
+
+
+
+
+
+
+
